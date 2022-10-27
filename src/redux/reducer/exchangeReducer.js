6f820ffc4668368633202ec0c80600ac1c74a5e2
greatt8696 <88331836+greatt8696@users.noreply.github.com>
2022-10-27 11:41:14 +0900

    123

--- src/redux/reducer/exchangeReducer.js
+++ src/redux/reducer/exchangeReducer.js
@@ -8,14 +8,12 @@ const exchangeReducer = (state = initState, action) => {
 
   switch (type) {
     case "ADD_TRANSACTION":
-      console.log("@@@@", payload);
       return {
         ...state,
         exchangeTransactions: [payload, ...state.exchangeTransactions],
       };
 
     case "CHANGE_TRANSACTION":
-      console.log("@@@@", payload);
       return {
         ...state,
         orderbooks: payload,
