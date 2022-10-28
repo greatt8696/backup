ed66548197719f20c8af283f3ff2919698d35851
greatt8696 <88331836+greatt8696@users.noreply.github.com>
2022-10-28 20:54:50 +0900

    123

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -3,11 +3,13 @@ import loginReducer from "./loginReducer";
 import transferAssetReducer from "./transferAssetReducer";
 import exchangeReducer from "./exchangeReducer";
 import coinReducer from "./coinReducer";
+import userReducer from "./userReducer";
 
 const rootReducer = combineReducers({
   loginReducer,
   transferAssetReducer,
   exchangeReducer,
+  userReducer,
   coinReducer,
 });
 
